79a1bcd46358de8779cb0e7bdaed2cf50dc6b395
REBEKAH LEONARD <shelbek@gmail.com>
2023-03-30 13:53:49 -0400

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
+import Welcome from './components/Welcome';
+import Profile from './components/Profile';
+import EditProfile from './components/EditProfile';
+import AddRecipe from './components/AddRecipe';
+import RecipePage from './components/RecipePage';
+import ContinentPage from './components/ContinentPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}>
+            <Route index element={<Welcome />} />
+            <Route path="/signin" element={ <SignIn /> } />
+            <Route path="/signup" element={ <SignUp /> } />
+            <Route path="/profile" element={ <Profile /> } />
+            <Route path="/profile/:id" element={ <EditProfile /> } />
+            <Route path='/recipe/add' element={ <AddRecipe />}/>
+            <Route path='/recipe' elemet={ <RecipePage /> }/>
+            <Route path='/continent' element={ <ContinentPage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
