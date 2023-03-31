e11750804bee60d89778b1e7f420578f0047a5ee
REBEKAH LEONARD <shelbek@gmail.com>
2023-03-31 13:33:58 -0400

    Started Home component. Added Outlet.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Navbar, Stack } from 'react-bootstrap';
+import { Outlet } from 'react-router';
 
 const Home = () => {
 
     return (
         <div>
+            <Navbar>
 
+            </Navbar>
+            <Stack>
+                <Outlet />
+            </Stack>
         </div>
 
     )
