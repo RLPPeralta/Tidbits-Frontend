eeb05d40bb6e9e2e3af5fd4ba4902189b02daca3
REBEKAH LEONARD <shelbek@gmail.com>
2023-04-03 17:21:26 -0400

    added basic navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Navbar, Stack } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Stack } from 'react-bootstrap';
 import { Outlet } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
     return (
         <div>
             <Navbar>
-
+                <Nav>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/signup" className="nav-link">Sign Up</Link>
+                <Link to="/signin" className="nav-link">Login</Link>
+                <NavDropdown title="Select Continent" id="basic-nav-dropdown">
+                    <Link to="/africa" className="nav-link">Africa</Link>
+                    <Link to="/antarctica" className="nav-link">Anarctica</Link>
+                    <Link to="/asia" className="nav-link">Asia</Link>
+                    <Link to="/australia" className="nav-link">Australia</Link>
+                    <Link to="/europe" className="nav-link">Europe</Link>
+                    <Link to="/northamerica" className="nav-link">North America</Link>
+                    <Link to="/southamerica" className="nav-link">South America</Link>
+                </NavDropdown>
+                </Nav>
             </Navbar>
             <Stack>
                 <Outlet />
