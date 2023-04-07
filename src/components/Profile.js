211889c1218d25e976c16cc4defcb6b5732d2bdf
Rachel Provost <rachelnp@outlook.com>
2023-04-07 11:11:44 -1000

    fixed profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,38 +8,38 @@ const Profile = () => {
     let params = useParams()
     let navigate = useNavigate()
   
-    let { getCurrentUser, editUser } = useContext(UserContext)
+    let { getUser, editUser } = useContext(UserContext)
     let [ userProfile, setUserProfile ] = useState()
 
     useEffect(() => {
       async function fetch() {
-        await getCurrentUser(params.userId)
+        await getUser(params.userId)
           .then((user) => setUserProfile(user))
       }
       fetch()
     }, [params.userId]);
 
-    // function loading() {
-    //     return <div className="w-25 text-center"><Spinner animation="border" /></div>
-    //   }
+    function loading() {
+        return <div className="w-25 text-center"><Spinner animation="border" /></div>
+      }
 
-    // function profileComponent() {
-    //     let { firstName, lastName, email, bio, continent, userId} = userProfile
-    //     return (
-    //     <div>
+    function profileComponent() {
+        let { firstName, lastName, email, bio, continent, userId} = userProfile
+        return (
+        <div>
 
-    //           <h1>Welcome back, {firstName}</h1><br></br>
-    //           <h2>Bio: {bio}</h2>
-    //           <p>Continent: {continent}</p><br></br>
+              <h1>Welcome back, {firstName}</h1><br></br>
+              <p>Bio: {bio}</p>
+              <p>Continent: {continent}</p><br></br>
 
-    //           <Link to={`/editprofile/${userId}`} className="btn btn-primary mx-3">Edit My Profile</Link>
-    //           <Link to={`/recipe/add`} className="btn btn-primary mx-3">Add a Recipe</Link>
+              <Link to={`/editprofile/${userId}`} className="btn btn-primary mx-3">Edit My Profile</Link>
+              <Link to={`/recipe/add`} className="btn btn-primary mx-3">Add a Recipe</Link>
 
-    //           <Button className="btn btn-primary mx-3" variant="success" onClick={() => navigate('/')}>Go Back</Button>
-    //       </div>
-    //     )
-    //   }
-    // return  userProfile ? profileComponent() : loading();
+              <Button className="btn btn-primary mx-3" variant="success" onClick={() => navigate('/')}>Go Back</Button>
+          </div>
+        )
+      }
+    return  userProfile ? profileComponent() : loading();
 };
 
 export default Profile;
