e82bf003a8b2d284706a06db029e60f17e2cac37
REBEKAH LEONARD <shelbek@gmail.com>
2023-04-10 21:37:01 -0400

    Got the recipes from europe to show up on europe's page

--- src/components/Europe.js
+++ src/components/Europe.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Card, Image, Row, Stack } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
+import RecipeContext from '../contexts/RecipeContext';
+
 
 const Europe = () => {
 
-    return (
-        <div>
+    let navigate = useNavigate()
 
+    let { getAllRecipes, deleteRecipe } = useContext(RecipeContext)
+
+function getRecipes(recipe){
+    if (recipe === null) return
+    return recipe.map((recipes) => {
+        if(recipes.continent === "Europe"){
+            return(
+                <Card key={recipes.id}>
+                <Card.Body key={recipes.id}>
+                    <Card.Img variant="top" src={recipes.image}/>
+                    <Card.Title>{recipes.recipe} from {recipes.continent}</Card.Title>
+                    <Card.Subtitle><strong>Ingredients:</strong> {recipes.ingredients} </Card.Subtitle>
+                    <Card.Subtitle><strong>Instructions:</strong> {recipes.instructions} </Card.Subtitle>
+                </Card.Body>
+            </Card>)
+            }}
+        )
+    
+}
+    return (
+        <div> 
+            
+            <Stack>
+                <Row xs={1} md={5} className="g-4">
+                    <RecipeContext.Consumer>
+                        {({recipe}) => (
+                            getRecipes(recipe)
+                            )}
+                    </RecipeContext.Consumer>
+                </Row>
+            </Stack>
         </div>
 
     )
--- src/contexts/RecipeProvider.js
+++ src/contexts/RecipeProvider.js
@@ -24,12 +24,6 @@ export const RecipeProvider = (props) => {
         });
     }
 
-    // function getRecipeNorthAmerica(continent) {
-    //     return axios.get(baseUrl + continent).then(response => {
-    //         return new Promise (resolve => resolve(response.data));
-    //     });
-    // }
-
     function addRecipe(recipe) {
         console.log('addRecipe()')        
         let myHeaders = {
@@ -74,7 +68,6 @@ export const RecipeProvider = (props) => {
             recipe,
             getAllRecipes,
             getRecipe,
-            // getRecipeNorthAmerica,
             addRecipe,
             editRecipe,
             deleteRecipe
