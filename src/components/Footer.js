36bdfce596b20ef15245f9149aeb30c1fa4d637b
Rachel Provost <rachelnp@outlook.com>
2023-04-18 21:13:06 -1000

    created basic footer. WIP. Pushing what is completed for now

---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import '../css/Footer.css';
+
+// const Footer = () => {
+//     const year = new Date().getFullYear();
+  
+//     return  <div className="footer">
+//     <footer>{`Copyright © Upbeat Code ${year}`}</footer>
+//   </div> 
+//   };
+  
+// export default Footer;
+
+
+import {
+  AiFillFacebook,
+  AiFillTwitterCircle,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <div className="footer-content">
+          <h3
+            style={{
+              fontFamily: "DM Serif Display",
+              fontSize: "2em"
+            }}
+          >
+            <em className='footer-company-name'>Tidbits</em><br></br>
+            <b className='slogan'>For Food Lovers</b><br></br>
+            <b className='trademark'>Tidbits is a registered company under [insert fake name]</b>
+          </h3>
+          <div className="sub">
+            <div>
+              <b>Company</b>
+              <p>About</p>
+              <p>Blog</p>
+            </div>
+            <div>
+              <b>For Foodies</b>
+              <p>Community</p>
+            </div>
+            <div>
+              <b>For You</b>
+              <p>Privacy</p>
+              <p>Terms</p>
+            </div>
+            <div>
+              <b>Social links</b>
+              <div>
+                <AiFillFacebook />
+                <AiFillTwitterCircle />
+                <AiFillInstagram />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
